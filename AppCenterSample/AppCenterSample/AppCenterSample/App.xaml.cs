e67adaf33a36a542280a4a90a706d6abce98589c
Luis <luispenholato@hotmail.com>
2018-06-24 14:24:46 -0300

    project

---
+++ AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Prism;
+using Prism.Ioc;
+using AppCenterSample.ViewModels;
+using AppCenterSample.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism.Autofac;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppCenterSample
+{
+    public partial class App : PrismApplication
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=05656c3a-469d-4fe5-83e4-9ca9db8d1d7e;" +
+                            "uwp={Your UWP App secret here};" +
+                            "ios=7a269aa4-9df3-4120-a711-4c570f9fa9fb", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+    }
+}
